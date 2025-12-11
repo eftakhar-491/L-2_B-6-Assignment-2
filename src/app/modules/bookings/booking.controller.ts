c8676193f84699a885b96e9b5dd6adefba14f982
MD EFTAKHARUL ISLAM KHAN <eftt042@gmail.com>
2025-12-11 23:34:24 +0600

    all route done

--- src/app/modules/bookings/booking.controller.ts
+++ src/app/modules/bookings/booking.controller.ts
@@ -1,9 +1,50 @@
+import { StatusCodes } from "http-status-codes";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 import { BookingsService } from "./booking.service";
 
 const createBookings = catchAsync(async (req, res) => {
-  await BookingsService.createBookings(req.body);
-  res.status(200).json({ message: "Booking created successfully" });
+  const bookedData = await BookingsService.createBookings(req.body);
+  sendResponse(res, {
+    statusCode: StatusCodes.CREATED,
+    success: true,
+    message: "Booking created successfully",
+    data: bookedData,
+  });
 });
 
-export const BookingsControllers = { createBookings };
+const getBookings = catchAsync(async (req, res) => {
+  const isAdmin = req.user?.role === "admin";
+  const bookings = await BookingsService.getBookings(isAdmin, req.user?.id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Bookings retrieved successfully",
+    data: bookings,
+  });
+});
+
+const updateBookings = catchAsync(async (req, res) => {
+  const bookingId = parseInt(req.params.bookingId);
+  const user = { id: req.user?.id, role: req.user?.role };
+  const updatedBooking = await BookingsService.updateBookings(
+    bookingId,
+    req.body.status,
+    user
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message:
+      req.body.status === "cancelled"
+        ? "Booking cancelled successfully"
+        : "Booking marked as returned. Vehicle is now available",
+    data: updatedBooking,
+  });
+});
+
+export const BookingsControllers = {
+  createBookings,
+  getBookings,
+  updateBookings,
+};
--- src/app/modules/bookings/booking.route.ts
+++ src/app/modules/bookings/booking.route.ts
@@ -6,6 +6,20 @@ import { BookingsControllers } from "./booking.controller";
 
 const router = Router();
 
-router.post("/", checkAuth("admin"), BookingsControllers.createBookings);
+router.post(
+  "/",
+  checkAuth("admin", "customer"),
+  BookingsControllers.createBookings
+);
+router.get(
+  "/",
+  checkAuth("admin", "customer"),
+  BookingsControllers.getBookings
+);
+router.put(
+  "/:bookingId",
+  checkAuth("admin", "customer"),
+  BookingsControllers.updateBookings
+);
 
 export const BookingsRoutes = router;
